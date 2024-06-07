1e955bc7e36175547ecb4defb2c83f0ca7720392
Tambacu <ivanmatheus29@gmail.com>
2024-06-06 20:28:14 -0400

    second activity youtube

--- app/src/main/java/com/uninorte/laera2/SecondActivity.kt
+++ app/src/main/java/com/uninorte/laera2/SecondActivity.kt
@@ -1,12 +1,25 @@
 package com.uninorte.laera2
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
 
 class SecondActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val curso1 = findViewById<ImageButton>(R.id.imageButton_python)
+        curso1.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6"))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("force_fullscreen", true)
+            startActivity(intent)
+
+
+        }
     }
 
 }
